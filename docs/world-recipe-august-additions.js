770eb01300577b76baec8c16d5c8cf15b4697ba5
ablack2225 <marketer.amy@gmail.com>
2026-08-02 18:29:03 -0500

    Update brisket bowl search metadata

--- docs/world-recipe-august-additions.js
+++ docs/world-recipe-august-additions.js
@@ -16,14 +16,14 @@
 
   if (!recipes.some((item) => item.path === 'recipes/mexican/chipotle-guajillo-lime-brisket-with-blackened-fresh-plum-salsa.html')) {
     recipes.push({
-      title: 'Chipotle-Guajillo Lime Brisket with Blackened Fresh-Plum Salsa',
+      title: 'Chipotle-Guajillo Lime Brisket Bowls with Blackened Fresh-Plum Salsa',
       path: 'recipes/mexican/chipotle-guajillo-lime-brisket-with-blackened-fresh-plum-salsa.html',
       collection: 'House Recipes',
       culture: 'Mexican Inspired',
-      category: 'Main Dish',
+      category: 'Complete Bowl',
       status: 'Flavorweaver Test Recipe',
-      ingredients: ['thin-sliced brisket', 'brisket', 'dried guajillo chiles', 'chipotle in adobo', 'orange juice', 'lime zest', 'lime juice', 'less-sodium soy sauce', 'honey', 'cumin', 'coriander', 'Mexican oregano', 'smoked paprika', 'black pepper', 'MSG', 'sweet onion', 'garlic', 'fresh plums', 'Roma tomatoes', 'jalapeno', 'fresh hot red Mexican chile', 'green onions', 'cilantro', 'rice vinegar', 'white vinegar', 'olive oil', 'brisket tallow'],
-      tags: ['Mexican-Inspired', 'mexican-inspired', 'chipotle guajillo brisket', 'lime brisket', 'thin-sliced brisket', 'blackened fresh plum salsa', 'fresh plum salsa', 'food processor salsa', 'skillet charred salsa', 'guajillo honey reduction', 'sandwich filling', 'rice bowl protein', 'wrap filling', 'Flavorweaver Test Recipe', 'html page']
+      ingredients: ['thin-sliced brisket', 'brisket', 'dried guajillo chiles', 'chipotle in adobo', 'orange juice', 'lime zest', 'lime juice', 'less-sodium soy sauce', 'honey', 'cumin', 'coriander', 'Mexican oregano', 'smoked paprika', 'black pepper', 'MSG', 'sweet onion', 'garlic', 'fresh plums', 'Roma tomatoes', 'jalapeno', 'fresh hot red Mexican chile', 'green onions', 'cilantro', 'rice vinegar', 'white vinegar', 'long-grain rice', 'chicken broth', 'bell pepper', 'corn', 'brisket tallow', 'fresh corn tortillas'],
+      tags: ['Mexican-Inspired', 'mexican-inspired', 'chipotle guajillo brisket bowl', 'cilantro lime rice', 'charred corn', 'bell pepper onion corn', 'blackened fresh plum salsa', 'fresh plum salsa', 'food processor salsa', 'skillet charred salsa', 'guajillo honey reduction', 'complete bowl', 'rice bowl', 'corn tortilla option', 'Flavorweaver Test Recipe', 'html page']
     });
   }
 })();

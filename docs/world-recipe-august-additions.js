ff905008523992abd19c76425dbcac5e922cfa28
ablack2225 <marketer.amy@gmail.com>
2026-08-03 13:34:15 -0500

    Index Korean smoky plum pork hoagies

--- docs/world-recipe-august-additions.js
+++ docs/world-recipe-august-additions.js
@@ -3,14 +3,14 @@
 
   if (!recipes.some((item) => item.path === 'recipes/korean/korean-smoky-plum-bbq-pork.html')) {
     recipes.push({
-      title: 'Korean Smoky-Plum BBQ Pork',
+      title: 'Korean Smoky-Plum BBQ Pork Hoagies',
       path: 'recipes/korean/korean-smoky-plum-bbq-pork.html',
       collection: 'House Recipes',
       culture: 'Korean Inspired',
-      category: 'Main Dish',
+      category: 'Complete Sandwich',
       status: 'Flavorweaver Test Recipe',
-      ingredients: ['pork butt', 'fresh plums', 'gochujang', 'less-sodium soy sauce', 'rice vinegar', 'honey', 'brown sugar', 'sweet onion', 'garlic', 'fresh ginger', 'green onions', 'gochugaru', 'black pepper', 'MSG', 'toasted sesame oil', 'chicken broth', 'tomato paste', 'smoked paprika'],
-      tags: ['Korean-Inspired', 'korean-inspired', 'Korean BBQ pork', 'smoky plum pork', 'fresh plum glaze', 'gochujang', 'pork butt', 'sear braise lacquer', 'sticky charred pork', 'sandwich filling', 'rice bowl protein', 'lettuce wraps', 'freezer friendly', 'Flavorweaver Test Recipe', 'html page']
+      ingredients: ['pork butt', 'fresh plums', 'gochujang', 'less-sodium soy sauce', 'rice vinegar', 'honey', 'brown sugar', 'sweet onion', 'garlic', 'fresh ginger', 'green onions', 'gochugaru', 'black pepper', 'MSG', 'toasted sesame oil', 'chicken broth', 'tomato paste', 'smoked paprika', 'mayonnaise', 'lime juice', 'red onion', 'carrot', 'jalapeno', 'green cabbage', 'sesame seeds', 'homemade hoagie rolls', 'butter'],
+      tags: ['Korean-Inspired', 'korean-inspired', 'Korean BBQ pork hoagie', 'smoky plum pork', 'fresh plum glaze', 'gochujang', 'pull apart pork', 'coarse pulled pork', 'spicy gochujang mayo', 'quick pickles', 'sesame cabbage crunch', 'homemade hoagie roll', 'sear braise pull lacquer char', 'complete sandwich', 'featured meal', 'Flavorweaver Test Recipe', 'html page']
     });
   }
 
